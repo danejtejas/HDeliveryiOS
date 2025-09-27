2edc472e6b4184e56482a25e4e78edb50db6a424
Tejas <danej.tejash0@gmail.com>
2025-09-27 20:02:45 +0800

    Initial Commit

---
+++ HDelivery/HDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDeliveryApp.swift
+//  HDelivery
+//
+//  Created by user286520 on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
